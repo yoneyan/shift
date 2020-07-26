8055cfe20104f529765cb98273381223ca346e16
Yuto Yoneda <yoneda@local.bgp.ne.jp>
2020-07-26 19:06:31 +0900

    ページの追加

---
+++ lib/pages/shiftRegistration.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calendar_carousel/classes/event.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+
+import '../drawer.dart';
+
+class ShiftRegistrationPage extends StatelessWidget {
+  static const String routeName = '/shiftRegistration';
+
+  DateTime _currentDate = DateTime.now();
+
+  void onDayPressed(DateTime date, List<Event> events) {
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text("シフト登録"),
+        ),
+        drawer: AppDrawer(),
+        body: Container(
+          child: CalendarCarousel<Event>(
+              onDayPressed: onDayPressed,
+              weekendTextStyle: TextStyle(color: Colors.red),
+              thisMonthDayBorderColor: Colors.grey,
+              weekFormat: false,
+              height: 450.0,
+              selectedDateTime: _currentDate,
+              daysHaveCircularBorder: false,
+              customGridViewPhysics: NeverScrollableScrollPhysics(),
+              markedDateShowIcon: true,
+              markedDateIconMaxShown: 2,
+              todayTextStyle: TextStyle(
+                color: Colors.blue,
+              ),
+              markedDateIconBuilder: (event) {
+                return event.icon;
+              },
+              todayBorderColor: Colors.green,
+              markedDateMoreShowTotal: false),
+        ));
+  }
+}
