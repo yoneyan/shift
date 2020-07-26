cfaaacbbe374dd63ff9b44a438778d4961ef83a3
Yuto Yoneda <yoneda@local.bgp.ne.jp>
2020-07-27 05:38:53 +0900

    ログイン有無処理の一時無効化

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shift/pages/shift.dart';
+import 'package:shift/pages/shiftRegistration.dart';
 
-import 'auth.dart';
-import 'drawer.dart';
+import 'package:shift/services/auth.dart';
 import 'login.dart';
 import 'pages/shift.dart';
 import 'routes.dart';
@@ -14,38 +14,12 @@ void main() {
       primarySwatch: Colors.blue,
       visualDensity: VisualDensity.adaptivePlatformDensity,
     ),
-    home: RootPage(title: 'Shift', auth: new Auth()),
+    home: LoginPage(),
+//    home: RootPage(title: 'Shift', auth: new Auth()),
     routes: {
       Routes.shift: (context) => ShiftPage(),
+      Routes.shiftRegistration: (context) => ShiftRegistrationPage(),
 //      "/shift": (context) => ShiftPage(),
     },
   ));
-}
-
-class RootPage extends StatefulWidget {
-  RootPage({Key key, this.title, this.auth}) : super(key: key);
-  final Auth auth;
-  final String title;
-
-  @override
-  _RootPageState createState() => _RootPageState();
-}
-
-class _RootPageState extends State<RootPage> {
-  bool _isLogin = false;
-
-  initState() {
-    super.initState();
-    widget.auth.checkLogin().then((status) => _isLogin = status);
-    print(_isLogin);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Shift",
-//      theme: ThemeData.dark(),
-      home: _isLogin ? ShiftPage() : LoginPage(),
-    );
-  }
 }
