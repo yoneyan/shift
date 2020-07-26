5aaa7cc78ae5e4b5550bbc0306ae6a908d19ca76
Yuto Yoneda <yoneda@local.bgp.ne.jp>
2020-07-27 05:37:07 +0900

    パスワード変更機能の追加

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:shift/services/auth.dart';
 
-import '../drawer.dart';
+class SettingPage extends StatefulWidget {
+  SettingPage();
 
-class SettingPage extends StatelessWidget {
-  static const String routeName = '/shift';
+  @override
+  _SettingPage createState() => _SettingPage();
+}
+
+class _SettingPage extends State<SettingPage> {
+  static const String routeName = '/setting';
+
+  final Auth auth = new Auth();
+
+  final emailInputController = new TextEditingController();
+  final passInputController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    auth.currentUser().then((value) => emailInputController.text = value.email);
+
     return new Scaffold(
       appBar: AppBar(
         title: Text("Setting"),
       ),
-      body: Center(
-        child: Text("Setting Pages"),
+      body: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Text("パスワードの変更"),
+            splashColor: Colors.blue,
+            shape: UnderlineInputBorder(),
+            onPressed: () {
+              auth.changePassword(emailInputController.value.text);
+            },
+          ),
+//          RaisedButton(
+//            child: Text("確認メールの送信"),
+//            splashColor: Colors.blueGrey,
+//            shape: UnderlineInputBorder(),
+//            onPressed: () {},
+//          ),
+        ],
       ),
     );
   }
-
 }
