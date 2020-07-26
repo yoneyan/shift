62bfe80798e9bb0ab1a16ca04391b46df71266ec
Yuto Yoneda <yoneda@local.bgp.ne.jp>
2020-07-26 19:05:40 +0900

    ページのrouteの追加

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,8 @@
 import 'package:shift/pages/shift.dart';
+import 'package:shift/pages/shiftRegistration.dart';
 
 class Routes {
   static const String shift = ShiftPage.routeName;
-//  static const String events = EventsPage.routeName;
+  static const String shiftRegistration = ShiftRegistrationPage.routeName;
 //  static const String notes = NotesPage.routeName;
 }
