663ecefbff274a7df610f41f7c062d221851e1a8
Yuto Yoneda <yoneda@local.bgp.ne.jp>
2020-07-28 00:56:05 +0900

    シフト登録ページのデザインの追加

--- lib/pages/shiftRegistration.dart
+++ lib/pages/shiftRegistration.dart
@@ -1,45 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+import 'package:shift/pages/shiftRegistrationDetail.dart';
 
 import '../drawer.dart';
 
-class ShiftRegistrationPage extends StatelessWidget {
+class ShiftRegistrationPage extends StatefulWidget {
+  @override
+  _ShiftRegistrationPageState createState() => _ShiftRegistrationPageState();
+}
+
+class _ShiftRegistrationPageState extends State<ShiftRegistrationPage> {
   static const String routeName = '/shiftRegistration';
 
   DateTime _currentDate = DateTime.now();
-
-  void onDayPressed(DateTime date, List<Event> events) {}
+  DateTime selectDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
+    void onDayPressed(DateTime date, List<Event> events) {
+      setState(() {
+        selectDate = date;
+      });
+    }
+
+    void detailPages(DateTime date) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ShiftRegistrationDetailPage()));
+    }
+
     return new Scaffold(
         appBar: AppBar(
-          title: Text("シフト登録"),
+          title: const Text("シフト登録"),
         ),
         drawer: AppDrawer(),
         body: Column(children: <Widget>[
           Container(
             child: CalendarCarousel<Event>(
-                onDayPressed: onDayPressed,
+                headerMargin: EdgeInsets.symmetric(),
+                locale: 'JA',
+                onDayPressed: (DateTime date, List<Event> events) {
+                  onDayPressed(date, events);
+                },
                 weekendTextStyle: TextStyle(color: Colors.red),
-                thisMonthDayBorderColor: Colors.grey,
+//                thisMonthDayBorderColor: Colors.grey,
                 weekFormat: false,
-                height: 450.0,
+                height: 400.0,
                 selectedDateTime: _currentDate,
                 daysHaveCircularBorder: false,
                 customGridViewPhysics: NeverScrollableScrollPhysics(),
                 markedDateShowIcon: true,
                 markedDateIconMaxShown: 2,
                 todayTextStyle: TextStyle(
-                  color: Colors.blue,
+                  color: Colors.blueAccent,
                 ),
                 markedDateIconBuilder: (event) {
                   return event.icon;
                 },
-                todayBorderColor: Colors.green,
+                todayBorderColor: Colors.lightGreen,
+                onDayLongPressed: (DateTime date) {
+                  detailPages(date);
+                },
                 markedDateMoreShowTotal: false),
           ),
+          Row(
+            children: <Widget>[
+              Text(' '),
+              RaisedButton(
+                child: Text("曜日一括登録"),
+                color: Colors.orange,
+                textColor: Colors.white,
+                onPressed: () {
+                  selectDate = DateTime.now();
+                },
+              ),
+              Text(' '),
+              RaisedButton(
+                child: Text("シフト登録"),
+                color: Colors.orange,
+                textColor: Colors.white,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Text(selectDate.year.toString() +
+              '年' +
+              selectDate.month.toString() +
+              '月' +
+              selectDate.day.toString() +
+              '日'),
         ]));
   }
 }
